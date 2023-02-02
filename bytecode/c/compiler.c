66bc5c242c4213b9f83f7db4c8adb6cd171282b5
Kashif Soofi <kashif.soofi@deliveroo.co.uk>
2023-02-02 13:41:59 +0000

    expression statement

--- bytecode/c/compiler.c
+++ bytecode/c/compiler.c
@@ -265,6 +265,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -278,6 +284,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
